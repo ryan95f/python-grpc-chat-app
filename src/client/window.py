50ce056a8cd6723eed897d89c64615ef3496e8ff
Ryan95Z <Ryan95f@gmail.com>
2020-12-06 10:38:25 +0000

    increased the size of assets. Layed out the items for making a connection

--- src/client/window.py
+++ src/client/window.py
@@ -10,13 +10,17 @@ class Window(tk.Tk):
         self.client = GrpcClient()
 
     def __setup_widgets(self):
-        self.text_input = tk.Entry(self)
-        self.text_input.grid(column=0, row=0, columnspan=3)
+        self.username_input = tk.Entry(self, width=50)
+        self.username_input.grid(column=0, row=0, columnspan=5)
 
-        self.connect_btn = tk.Button(self, text='Connect', command=self.__connect_client)
-        self.connect_btn.grid(row=0, column=5)
+        self.connect_btn = tk.Button(self, text='Connect', width=15, command=self.__connect_client)
+        self.connect_btn.grid(row=0, column=6)
+
+        self.is_connected_msg = tk.StringVar(value='Not Connected')
+        self.connection_status_label = tk.Label(self, width=15, textvariable=self.is_connected_msg)
+        self.connection_status_label.grid(row=0, column=7)
 
     def __connect_client(self):
-        username = self.text_input.get()
+        username = self.username_input.get()
         result = self.client.connect(username)
-        print(result)
+        self.is_connected_msg.set('Connected')

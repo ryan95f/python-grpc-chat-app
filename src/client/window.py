87d608d257e57065280c6823c68e2afb79483cfd
Ryan95Z <Ryan95f@gmail.com>
2020-12-07 19:35:02 +0000

    moved and refined the window class methods

--- src/client/window.py
+++ src/client/window.py
@@ -12,6 +12,7 @@ class Window(tk.Tk):
         self.title('grpc chat!')
         self.grid()
         self.resizable(False, False)
+
         self.__client = GrpcClient()
         self.__setup_widgets()
 
@@ -23,7 +24,7 @@ class Window(tk.Tk):
         self.username_input = tk.Entry(self, width=50)
         self.username_input.grid(row=0, column=0, columnspan=3, sticky='we')
 
-        self.connect_btn = tk.Button(self, text='Connect', width=15, command=self.__connect_client)
+        self.connect_btn = tk.Button(self, text='Connect', width=15, command=self.__btn_action_toggle_client_connection)
         self.connect_btn.grid(row=0, column=4)
 
         self.is_connected_msg = tk.StringVar(value='Not Connected')
@@ -37,50 +38,73 @@ class Window(tk.Tk):
         self.chat_box = tk.Text(self, height=5)
         self.chat_box.grid(row=2, column=0, columnspan=3)
 
-        self.send_btn = tk.Button(self, text='Send', height=5, width=20, command=self.__send_message)
+        self.send_btn = tk.Button(self, text='Send', height=5, width=20, command=self.__btn_action_send_message)
         self.send_btn.grid(row=2, column=4, columnspan=2, sticky='we')
         self.send_btn['state'] = 'disabled'
 
-        self.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.protocol("WM_DELETE_WINDOW", self.__on_close)
 
-    def on_close(self):
-        if self.__client.is_connected:
-            self.__client.disconnect()
-        self.destroy()
-
-    def __connect_client(self):
+    def __btn_action_toggle_client_connection(self):
         if self.__client.is_connected:
             self.__disconnect_from_server()
+            self.__clear_chat_messages()
         else:
             self.__connect_to_server()
 
     def __disconnect_from_server(self):
         self.__client.disconnect()
         self.is_connected_msg.set('Not Connected')
+        self.connection_status_label.configure(foreground='red')
+        self.connect_btn.configure(text='Connect')
+        self.send_btn['state'] = 'disabled'
 
     def __connect_to_server(self):
         username = self.username_input.get()
         self.user = self.__client.connect(username)
         self.is_connected_msg.set('Connected')
         self.send_btn['state'] = 'active'
-        Thread(target=self.__message_reciever_handler, args=(self.__client, )).start()
+        self.connection_status_label.configure(foreground='green')
+        self.connect_btn.configure(text='Disconnect')
 
-    def __message_reciever_handler(self, client):
-        response = client.subscribe_messages()
+        Thread(target=self.__message_reciever_handler).start()
+
+    def __message_reciever_handler(self):
+        response = self.__client.subscribe_messages()
         for message in response:
-            self.__add_message(message.username, message.message)
+            self.__display_chat_message(message.username, message.message)
 
-    def __add_message(self, username, message):
-            self.chat_mesages.insert(self.chat_mesages.size() + 1, '[{}] - {}'.format(username, message))
+    def __display_chat_message(self, username, message):
+            self.chat_mesages.insert(self.chat_mesages.size() + 1, '[{:10}] - {}'.format(username, message))
 
-    def __send_message(self):
-        message = self.chat_box.get('1.0','end-1c')
+    def __btn_action_send_message(self):
+        message = self.__get_users_message()
+        self.__clear_users_message_box()
+
+        grpc_message = self.__construct_message_payload(message)
+
+        self.__display_chat_message(self.user.username, message)
+        self.__client.send_message(grpc_message)
+
+    def __get_users_message(self):
+        return self.chat_box.get('1.0','end-1c')
+        
+    def __clear_users_message_box(self):
         self.chat_box.delete('1.0', 'end-1c')
 
-        message_to_send = chat_pb2.ChatMessage(
+    def __construct_message_payload(self, message):
+        return chat_pb2.ChatMessage(
             userId=self.user.userId,
             username=self.user.username,
             message=message
         )
-        self.__add_message(self.user.username, message)
-        self.__client.send_message(message_to_send)
+
+    def __on_close(self):
+        if self.__client.is_connected:
+            self.__client.disconnect()
+        self.destroy()
+
+    def __clear_chat_messages(self):
+        messages_start_index = 0
+        messsages_end_index = self.chat_mesages.size()
+        self.chat_mesages.delete(messages_start_index, messsages_end_index)
+

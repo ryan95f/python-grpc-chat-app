b22eaae1cf92284bc1d45d6884df2feac3f6a7af
Ryan95Z <Ryan95F@gmail.com>
2020-12-10 21:23:28 +0000

    Added docstrings to the server code

--- src/server/chat_service.py
+++ src/server/chat_service.py
@@ -13,19 +13,60 @@ class ChatService(chat_pb2_grpc.ChatServicer):
         self.stop_connection = False
 
     def connect(self, request, context):
+        """Register a user with the chat server
+
+        Args:
+            request: A grpc ChatUser object of the user attempting
+                     to connect to the server
+            context: Metadata concerning the request
+
+        Returns:
+            A grpc ChatUserConnected object with thier requested
+            username and a randomly generated user id
+        """
         user_id = random.randint(1, 10000)
         self.users[user_id] = request.username
         return chat_pb2.ChatUserConnected(username=request.username, userId=user_id)
 
     def disconnect(self, request, context):
+        """De-register a user from the chat server
+
+        Args:
+            request: A grpc ChatUserConnected object for the disconnecting user
+            context: Metadata concerning the request
+
+        Returns:
+            A grpc ChatUserDisconnect object with the
+            isDisconnected property set to true
+        """
         del self.users[request.userId]
         return chat_pb2.ChatUserDisconnect(isDisconnected=True)
 
     def sendMessage(self, request, context):
+        """Endpoint to recieve a message from the client
+
+        Args:
+            request: A grpc ChatMessage object containing the
+                     message and user information
+            context: Metadata concerning the request
+
+        Returns:
+            A grpc ChatMessage object with the user Id
+            and username from the user who send the message
+        """
         self.chats.append(request)
         return chat_pb2.ChatMessage(userId=request.userId, username=request.username)
 
     def subscribeMessages(self, request, context):
+        """Endpoint to subscribe to new messages from different users on the chat server.
+
+        Args:
+            request: A grpc ChatUserConnected object of the subscribing user
+            context: Metadata concerning the request
+
+        Returns:
+            A stream of grpc ChatMessage objects from other users on the server
+        """
         current_user_id = request.userId
         last_seen_message_index = 0
 
@@ -40,6 +81,15 @@ class ChatService(chat_pb2_grpc.ChatServicer):
         return self.users.get(user_id, None) is not None
 
     def subscribeActiveUsers(self, request, context):
+        """Endpoint to subscribe to active users on the chat server
+
+        Args:
+            request: A grpc ChatUserConnected object of the subscribing user
+            context: Metadata concerning the request
+
+        Returns:
+            A streaam of grpc ChatActiveUser objects
+        """
         current_hash = ""
         current_user_id = request.userId
 
@@ -50,4 +100,6 @@ class ChatService(chat_pb2_grpc.ChatServicer):
                 current_hash = md5_hash
                 for user_id, username in self.users.items():
                     if user_id != current_user_id:
-                        yield chat_pb2.ChatActiveUser(username=username, userId=user_id, currentHash=current_hash)
+                        yield chat_pb2.ChatActiveUser(username=username,
+                                                      userId=user_id,
+                                                      currentHash=current_hash)
